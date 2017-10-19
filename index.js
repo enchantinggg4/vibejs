33c09d6c9b48dbf4f3bd943f71838c6d3fc98578
enchanting <enchantinggg4@gmail.com>
2017-10-19 23:21:27 +0300

    again exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('babel-register')
 module.exports = {
     EntityStore: require('./src/EntityStore'),
-    Model: require('./src/Model').Model,
+    Model: require('./src/Model'),
     types: require('./src/Model').types,
     Struct: require('./src/Struct'),
 }
